8000a2cb2e71b27e5f26af3f30bad0e6c7a07975
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-05-12 11:12:33 +0700

    case 1

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -92,3 +92,9 @@ int main()
         cin >> choice;
 
         switch (choice)
+        {
+        case 1:
+            cout << "Enter the value too push: ";
+            cin >> value;
+            stack.push(value);//push the entered value into the stack
+            break;
